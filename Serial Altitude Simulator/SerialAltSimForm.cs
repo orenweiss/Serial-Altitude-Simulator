6317afb8935d7c1d44398275aafa3114ba18f99d
Oren Weiss <oren.weiss@faa.gov>
2017-03-20 11:00:03 -0400

    Add copyright header

--- Serial Altitude Simulator/SerialAltSimForm.cs
+++ Serial Altitude Simulator/SerialAltSimForm.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="SerialAltSimForm.cs" company="Federal Aviation Administration">
+//   Copyright (c) 2017, Federal Aviation Administration. All Rights Reserved.
+//
+//   Licensed under the Apache License, Version 2.0,
+//   you may not use this file except in compliance with one of the Licenses.
+// 
+//   You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an 'AS IS' BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -59,24 +76,6 @@ namespace Serial_Altitude_Simulator
                 baudRate = 1200,
                 dataBits = 7,
                 checksum = true });
-
-            /* 
-             * TODO: Add support for these protocols
-             *  this.altitudeProtocols.Add(ProtocolType.SHADIN_RMS, new AltitudeProtocol {
-             *      name = "SHADIN RMS Format",
-             *      stopBits = StopBits.One,
-             *      parity = Parity.None,
-             *      baudRate = 9600,
-             *      dataBits = 8,
-             *      checksum = true });
-             *      
-             *  this.altitudeProtocols.Add(ProtocolType.ARNAV, new AltitudeProtocol { 
-             *      stopBits = StopBits.One, 
-             *      parity = Parity.Odd, 
-             *      baudRate = 9600, 
-             *      dataBits = 8 });
-            */
-            
 
             foreach(AltitudeProtocol a in altitudeProtocols.Values)
             {
